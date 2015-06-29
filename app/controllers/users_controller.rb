52d24f1be1387bbb615f4d96130f9ae5346a16bc
vietanhbkaaa <pham.viet.anh@framgia.com>
2015-06-29 16:27:10 +0700

    Finish login-logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t "controller.welcome"
+      log_in @user
+      flash[:success] = t"controller.welcome"
       redirect_to @user
     else
       render :new
